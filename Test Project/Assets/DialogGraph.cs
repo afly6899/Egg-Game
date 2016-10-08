ca384f170f03097bb1776dad1e6723f52a52783a
avielmenter <avi@Aviels-MacBook.local>
2016-10-08 13:20:19 -0700

    Updating DialogGraph Class

--- Test Project/Assets/DialogGraph.cs
+++ Test Project/Assets/DialogGraph.cs
@@ -7,7 +7,6 @@ public class DialogGraph
 	private Dictionary<string, Scene> scenes;
 	private string currScene;
 
-
 	public string CurrentSpeaker
 	{
 		get;
@@ -16,8 +15,10 @@ public class DialogGraph
 
 	public string CurrentText
 	{
-		get;
-		private set;
+		get
+		{
+			return this.scenes [currScene].CurrentDialog;	
+		}
 	}
 
 	public List<string> CurrentChoices 
@@ -26,19 +27,42 @@ public class DialogGraph
 		private set;
 	}
 
+	public bool HasChoices
+	{
+		get
+		{
+			return CurrentChoices != null;
+		}
+	}
+
 	private void loadScenes(string sceneFile)
 	{
 		var lines = System.IO.File.ReadAllLines (sceneFile);
 		List<string> currSceneText = null;
 
+		Scene s = null;
+
 		foreach (var line in lines) 
 		{
-			
+			if (line.StartsWith ("[Scene ") && currSceneText != null) 
+			{
+				s = new Scene (currSceneText);
+				this.scenes.Add (s.Name, s);
+				currSceneText = new List<string> ();
+			}
+
+			currSceneText.Add (line);
 		}
+
+		s = new Scene(currSceneText);
+		this.scenes.Add(s.Name, s);
+
 	}
 
-	public DialogGraph(string sceneFile)
+	public DialogGraph(string sceneFile, string startScene)
 	{
+		loadScenes (sceneFile);
+		this.currScene = startScene;
 	}
 
 

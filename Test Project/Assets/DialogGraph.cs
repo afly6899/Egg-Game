4496dd84447efd0f310a638ce75b1c47dec7210b
avielmenter <avi@Aviels-MacBook.local>
2016-10-08 12:43:06 -0700

    Added DialogGraph Interface

---
+++ Test Project/Assets/DialogGraph.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DialogGraph
+{
+	private Dictionary<string, Scene> scenes;
+	private string currScene;
+
+
+	public string CurrentSpeaker
+	{
+		get;
+		private set;
+	}
+
+	public string CurrentText
+	{
+		get;
+		private set;
+	}
+
+	public List<string> CurrentChoices 
+	{
+		get;
+		private set;
+	}
+
+	private void loadScenes(string sceneFile)
+	{
+		var lines = System.IO.File.ReadAllLines (sceneFile);
+		List<string> currSceneText = null;
+
+		foreach (var line in lines) 
+		{
+			
+		}
+	}
+
+	public DialogGraph(string sceneFile)
+	{
+	}
+
+
+}

569ee01f3217a8c49b4654d8ec9581d189f05e49
afly6899 <aflai@uci.edu>
2016-10-29 17:32:28 -0700

    Added UI items for Character Portraits + fixes
    Currently supports 4 people on screen (the positions are denoted by the
    numbers in the Character Portrait panels).
    Fixed anchoring on name and dialogue boxes.
    Made the dialogue stuff translucent.

---
+++ Test Project/Assets/BackgroundManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BackgroundManager : MonoBehaviour {
+
+    public CanvasRenderer canvas;
+    public SpriteRenderer background;
+
+	// Use this for initialization
+	void Start () {
+        float worldScreenHeight = Camera.main.orthographicSize * 2;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        transform.localScale = new Vector3(
+            worldScreenWidth / background.sprite.bounds.size.x,
+            worldScreenHeight / background.sprite.bounds.size.y, 1);
+    }
+}
